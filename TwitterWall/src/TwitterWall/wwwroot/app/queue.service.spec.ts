b43382ffb021decd9241bf045651d8fea6dcd938
Marcus Redgrave-Close <mredgrave@scottlogic.co.uk>
2016-10-06 08:44:47 +0100

    added queue service

---
+++ TwitterWall/src/TwitterWall/wwwroot/app/queue.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from "@angular/core/testing";
+
+describe("Queue service tests", () => {
+
+    var fixture;
+    var component;
+    
+    beforeEach(() => {
+
+        component = {};
+        fixture = {};
+
+    });
+
+     it("should not allow more tweets than max set", () => {
+        fixture.detectChanges();
+        component.tweetStream.pushTweets();
+        expect(component.tweets.length).toBe(2);
+    });
+
+}
---
+++ TwitterWall/src/TwitterWall/wwwroot/app/queue.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from "@angular/core";
+import { Tweet } from "./tweet";
+
+@Injectable()
+export class Queue {
+
+    private queueSize: number;
+    private tweets: Tweet[];
+
+    constructor(queueSize: number) {
+        this.tweets = [];
+        this.queueSize < 0 ? this.queueSize = 0 : this.queueSize = queueSize;
+    }
+
+    getQueue(): Tweet[] {
+        return this.tweets;
+    }
+
+    getLength(): number {
+        return this.tweets.length;
+    }
+
+    getQueueSize(): number {
+        return this.queueSize;
+    }
+
+    setQueueSize(size: number) {
+        size < 0 ? this.queueSize = 0 : this.queueSize = size;
+    }
+
+    push(tweet: Tweet): void {
+        if ((this.getLength() < this.queueSize) || this.queueSize === 0) {
+            this.tweets.push(tweet);
+        }
+    }
+
+    peek(): Tweet {
+        return this.tweets[this.getLength() - 1];
+    }
+
+    pop(): Tweet {
+        return this.tweets.pop();
+    }
+}
